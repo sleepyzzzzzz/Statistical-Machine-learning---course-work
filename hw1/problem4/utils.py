c2aefaf38d6f52307c8c1c56db958ac407e66658
sleepyzzzzzz <chrisz881711@gmail.com>
2021-01-28 21:27:48 -0600

    hw1

---
+++ hw1/problem4/utils.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#############################################################################
+#  Normalize features of data matrix X so that every column has zero        #
+#  mean and unit variance                                                   #
+#     Input:                                                                #
+#     X: N x D where N is the number of rows and D is the number of         #
+#        features                                                           #
+#     Output: mu: D x 1 (mean of X)                                         #
+#          sigma: D x 1 (std dev of X)                                      #
+#         X_norm: N x D (normalized X)                                      #
+#############################################################################
+
+def feature_normalize(X):
+
+    ########################################################################
+    # TODO: modify the three lines below to return the correct values
+    ########################################################################
+    
+    
+  
+    ########################################################################
+    return X_norm, mu, sigma
+
+
