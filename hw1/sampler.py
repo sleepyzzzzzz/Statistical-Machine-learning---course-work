038dc84b1471ce1ef9cca3899e7fe41eeba85066
sleepyzzzzzz <chrisz881711@gmail.com>
2021-02-02 22:03:15 -0600

    finish Q1.10

--- hw1/sampler.py
+++ hw1/sampler.py
@@ -1,11 +1,12 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 class ProbabilityModel:
 
     # Returns a single sample (independent of values returned on previous calls).
     # The returned value is an element of the model's sample space.
-    def sample(self, size=[]):
-        pass
+    def sample(self):
+        return np.random.uniform(0, 1)
 
 
 # The sample space of this probability model is the set of real numbers, and
@@ -20,11 +21,17 @@ class UnivariateNormal(ProbabilityModel):
         self.sigma = sigma
 
     def sample(self):
-        model = ProbabilityModel()
-        u1 = model.sample()
-        u2 = model.sample()
-        x = self.mu + np.sqrt(-2 * np.log(u1)) * np.cos(2 * np.pi * u2) * self.sigma
-        return x
+        u1 = ProbabilityModel().sample()
+        u2 = ProbabilityModel().sample()
+        z = self.mu + np.sqrt(-2 * np.log(u1)) * np.cos(2 * np.pi * u2) * self.sigma
+        return z
+
+    def plot_his(self):
+        z = []
+        for i in range(-5000, 5000):
+            z.append(self.sample())
+        plt.hist(z)
+        plt.show()
 
     
 # The sample space of this probability model is the set of D dimensional real
@@ -57,7 +64,18 @@ class Categorical(ProbabilityModel):
         self.ap = ap
 
     def sample(self):
-        pass
+        cum = np.cumsum(self.ap)
+        u = ProbabilityModel().sample()
+        for i in range(len(self.ap)):
+            if u < cum[i]:
+                return i
+
+    def plot_his(self):
+        z = []
+        for i in range(5000):
+            z.append(self.sample())
+        plt.hist(z)
+        plt.show()
 
 
 # The sample space of this probability model is the union of the sample spaces of 
@@ -75,3 +93,9 @@ class MixtureModel(ProbabilityModel):
 
     def sample(self):
         pass
+
+if __name__ == '__main__':
+    U = UnivariateNormal(0, 1)
+    U.plot_his()
+    C = Categorical(np.array([0.1, 0.1, 0.3, 0.3, 0.2]))
+    C.plot_his()
