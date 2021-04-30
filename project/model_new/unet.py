3155ca04777b4c45f2b5b380dde13cd8cfcbcf6b
sleepyzzzzzz <chrisz881711@gmail.com>
2021-04-30 17:17:36 -0500

    Update unet.py

--- project/model_new/unet.py
+++ project/model_new/unet.py
@@ -7,7 +7,7 @@ class unet(nn.Module):
         super(unet, self).__init__()
         self.n_channels = n_channels
         self.n_classes = n_classes
-        self.encoder1 = nn.Sequential(nn.Conv2d(n_channels, 64, kernel_size = 3, stride = 1, padding = 1, bias=False),
+        self.conv_down1 = nn.Sequential(nn.Conv2d(n_channels, 64, kernel_size = 3, stride = 1, padding = 1, bias=False),
                                      nn.BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
                                      nn.ReLU(inplace=True),
                                      nn.Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=1, bias=False),
@@ -15,7 +15,7 @@ class unet(nn.Module):
                                      nn.Dropout2d(0.2),
                                      nn.ReLU(inplace=True))
         
-        self.encoder2 = nn.Sequential(nn.Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), padding=1, bias=False),
+        self.conv_down2 = nn.Sequential(nn.Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), padding=1, bias=False),
                                      nn.BatchNorm2d(128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
                                      nn.ReLU(inplace=True),
                                      nn.Conv2d(128, 128, kernel_size=(3, 3), stride=(1, 1), padding=1, bias=False),
@@ -23,7 +23,7 @@ class unet(nn.Module):
                                      nn.Dropout2d(0.2),
                                      nn.ReLU(inplace=True))
         
-        self.encoder3 = nn.Sequential(nn.Conv2d(128, 256, kernel_size=(3, 3), stride=(1, 1), padding=1, bias=False),
+        self.conv_down3 = nn.Sequential(nn.Conv2d(128, 256, kernel_size=(3, 3), stride=(1, 1), padding=1, bias=False),
                                      nn.BatchNorm2d(256, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
                                      nn.ReLU(inplace=True),
                                      nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=1, bias=False),
@@ -31,7 +31,7 @@ class unet(nn.Module):
                                      nn.Dropout2d(0.2),
                                      nn.ReLU(inplace=True))
         
-        self.encoder4 = nn.Sequential(nn.Conv2d(256, 512, kernel_size=(3, 3), stride=(1, 1), padding=1, bias=False),
+        self.conv_down4 = nn.Sequential(nn.Conv2d(256, 512, kernel_size=(3, 3), stride=(1, 1), padding=1, bias=False),
                                      nn.BatchNorm2d(512, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
                                      nn.ReLU(inplace=True),
                                      nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=1, bias=False),
@@ -39,14 +39,8 @@ class unet(nn.Module):
                                      nn.Dropout2d(0.2),
                                      nn.ReLU(inplace=True))
         
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
-        
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
-
-        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
-        
         self.conv_mid = nn.Sequential(nn.Conv2d(512, 1024, kernel_size=(3, 3), stride=(1, 1), padding=1, bias=False),
                                      nn.BatchNorm2d(1024, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
                                      nn.ReLU(inplace=True),   
@@ -54,28 +48,28 @@ class unet(nn.Module):
                                      nn.BatchNorm2d(1024, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
                                      nn.ReLU(inplace=True))
         
-        self.decoder4 = nn.Sequential(nn.Conv2d(1024, 512, kernel_size=(3, 3), stride=(1, 1), padding=1, bias=False),
+        self.conv_up4 = nn.Sequential(nn.Conv2d(1024, 512, kernel_size=(3, 3), stride=(1, 1), padding=1, bias=False),
                                      nn.BatchNorm2d(512, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
                                      nn.ReLU(inplace=True),
                                      nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=1, bias=False),
                                      nn.BatchNorm2d(512, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
                                      nn.ReLU(inplace=True))
         
-        self.decoder3 = nn.Sequential(nn.Conv2d(512, 256, kernel_size=(3, 3), stride=(1, 1), padding=1, bias=False),
+        self.conv_up3 = nn.Sequential(nn.Conv2d(512, 256, kernel_size=(3, 3), stride=(1, 1), padding=1, bias=False),
                                      nn.BatchNorm2d(256, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
                                      nn.ReLU(inplace=True),
                                      nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=1, bias=False),
                                      nn.BatchNorm2d(256, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
                                      nn.ReLU(inplace=True))
         
-        self.decoder2 = nn.Sequential(nn.Conv2d(256, 128, kernel_size=(3, 3), stride=(1, 1), padding=1, bias=False),
+        self.conv_up2 = nn.Sequential(nn.Conv2d(256, 128, kernel_size=(3, 3), stride=(1, 1), padding=1, bias=False),
                                      nn.BatchNorm2d(128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
                                      nn.ReLU(inplace=True),
                                      nn.Conv2d(128, 128, kernel_size=(3, 3), stride=(1, 1), padding=1, bias=False),
                                      nn.BatchNorm2d(128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
                                      nn.ReLU(inplace=True))
         
-        self.decoder1 = nn.Sequential(nn.Conv2d(128, 64, kernel_size=(3, 3), stride=(1, 1), padding=1, bias=False),
+        self.conv_up1 = nn.Sequential(nn.Conv2d(128, 64, kernel_size=(3, 3), stride=(1, 1), padding=1, bias=False),
                                      nn.BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
                                      nn.ReLU(inplace=True),
                                      nn.Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=1, bias=False),
@@ -90,26 +84,25 @@ class unet(nn.Module):
         
         self.upconv1 = nn.ConvTranspose2d(128, 128, kernel_size=(2, 2), stride=(2, 2))
         
-        self.conv1x1_out = nn.Conv2d(64, n_classes, kernel_size=(1, 1), stride=(1, 1))
+        self.conv_last = nn.Conv2d(64, n_classes, kernel_size=(1, 1), stride=(1, 1))
 
     def forward(self, x):
-        out = self.encoder1(x)
-        out = self.pool1(out)
-        out = self.encoder2(out)
-        out = self.pool2(out)
-        out = self.encoder3(out)
-        out = self.pool3(out)
-        out = self.encoder4(out)
-        out = self.pool4(out)
+        out = self.conv_down1(x)
+        out = self.pool(out)
+        out = self.conv_down2(out)
+        out = self.pool(out)
+        out = self.conv_down3(out)
+        out = self.pool(out)
+        out = self.conv_down4(out)
+        out = self.pool(out)
         out = self.conv_mid(out)
         out = self.upconv4(out)
-        out = self.decoder4(out)
+        out = self.conv_up4(out)
         out = self.upconv3(out)
-        out = self.decoder3(out)
+        out = self.conv_up3(out)
         out = self.upconv2(out)
-        out = self.decoder2(out)
+        out = self.conv_up2(out)
         out = self.upconv1(out)
-        out = self.decoder1(out)
-        out = self.conv1x1_out(out)
-
+        out = self.conv_up1(out)
+        out = self.conv_last(out)
         return out
